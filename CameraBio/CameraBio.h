fc34a0a18e76613ad027459b63d1b68eb13ca851
MatheusDomingos <matheus_cancao@hotmail.com>
2019-05-15 11:26:23 -0300

    Initial commit

---
+++ CameraBio/CameraBio.h
@@ -0,0 +1,21 @@
+//
+//  CameraBio.h
+//  CameraBio
+//
+//  Created by Daniel Zanelatto on 14/05/19.
+//  Copyright © 2019 Daniel Zanelatto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CameraBio.
+FOUNDATION_EXPORT double CameraBioVersionNumber;
+
+//! Project version string for CameraBio.
+FOUNDATION_EXPORT const unsigned char CameraBioVersionString[];
+
+#import <CameraBio/TestCam.h>
+
+// In this header, you should import all the public headers of your framework using statements like #import <CameraBio/PublicHeader.h>
+
+
